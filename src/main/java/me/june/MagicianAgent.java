7cc845c4f6319465c6a9001e0cb8ab6abf26470a
ces518 <pupupee9@gmail.com>
2019-10-17 22:00:19 +0900

    자바 에이전트

---
+++ src/main/java/me/june/MagicianAgent.java
@@ -0,0 +1,32 @@
+package me.june;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
+
+import java.lang.instrument.Instrumentation;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 17/10/2019
+ * Time: 8:57 오후
+ **/
+public class MagicianAgent {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        new AgentBuilder.Default()
+                .type(ElementMatchers.any())
+                .transform(new AgentBuilder.Transformer() {
+                    @Override
+                    public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule) {
+                        return builder.method(named("pullOut")).intercept(FixedValue.value("Rabbit"));
+                    }
+                }).installOn(inst);
+    }
+}
